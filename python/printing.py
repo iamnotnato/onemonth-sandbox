bb78808ad498970f9e20cd8b6ce0eaa97cada548
Alan Nato <alanwnato@gmail.com>
2023-10-28 21:42:14 +0300

    feat : multiline comments

---
+++ python/printing.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+print('''You would not believe your eyes
+If ten million fireflies
+Lit up the world as I fell asleep
+Cause they fill the open air
+And leave teardrops everywhere
+Youd think me rude but I would just stand and stare
+Id like to make myself believe that planet Earth turns slowly
+Its hard to say that Id rather stay awake when Im asleep
+Cause everything is never as it seems''')
