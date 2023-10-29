08b6bb4579d90ca032183a8b150040cca5b5be4d
Alan Nato <alanwnato@gmail.com>
2023-10-29 09:19:50 +0300

    fix : happy hour

--- python/happy_hour.py
+++ python/happy_hour.py
@@ -9,6 +9,56 @@ bars = ["Shoolbred's",
 
 people = ["Mattan",
           "Chris",
+	  "Jensen Ackles"
+"Andre 3000"
+"Naveen Andrews"
+"Jensen Atwood"
+"Tyler Bachtel"
+"Penn Badgley"
+"Simon Baker"
+"Christian Bale"
+"Eric Balfour"
+"Eric Bana"
+"Alex Band"
+"Antonio Banderas"
+"Ike Barinholtz"
+"Ben Barnes"
+"Eugen Bauder"
+"William Beckett"
+"Tyson Beckford"
+"David Beckham"
+"Jason Behr"
+"Jonathan Bennett "
+"Sam Bennett "
+"Dierks Bentley"
+"Gael Garcia Bernal"
+"Jon Bernthal"
+"Wilson Bethel "
+"Justin Bieber"
+"David Blaine"
+"James Blake"
+"Corbin Bleu"
+"Orlando Bloom"
+"Jon Bon Jovi"
+"Asher Book"
+"David Boreanaz"
+"Tom Bott"
+"Raoul Bova"
+"Bow Wow"
+"Marlon Brando"
+"Adam Brody"
+"Chris Brown"
+"Michel Brown"
+"Justin Bruening"
+"Austin Butler "
+"Gerard Butler"
+"Santiago Cabrera"
+"Bobby Cannavale "
+"Nick Cannon"
+"Robert Carmine"
+"Chris Carmack"
+"Ryan Carnes"
+"Anthony Catanzaro"
 	  "Gavin Belson",
           "that person you forgot to text back",
           "Kanye West",
@@ -17,10 +67,5 @@ people = ["Mattan",
 random_bar = random.choice(bars)
 random_person = random.choice(people)
 random_friend = random.choice(people)
-
-if random_friend != random_person:
- random_friend
-else:
- random_friend = random.choice(people)
 
 print(f"How about you go to {random_bar} with {random_person} and {random_friend}")
